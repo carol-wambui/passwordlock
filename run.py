7d38d6fbc5012aa87568a77e88b66c288581c32b
carol-wambui <carolwambuidaystar@gmail.com>
2020-11-23 13:56:36 +0300

    create save

--- run.py
+++ run.py
@@ -4,3 +4,10 @@ def demand_user(username,password):
     createuser = User(username,password)
     return createuser
 
+def enter_user (username,password):
+   save_user = User.user_recognised(username,password)
+   return save_user
+
+def save_user(user):
+    user.save_user()
+
